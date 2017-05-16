1247f87d328ccaae9744ee2e9e68473d2fe7a5dd
david.wei <david94.wei@gmail.com>
2017-05-16 11:07:05 +0200

    Create application header

---
+++ src/dare2share_header.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+
+
+COLOUR_RED		= "\033[1;31m"  
+COLOUR_BLUE		= "\033[1;34m"
+COLOUR_CYAN		= "\033[1;36m"
+COLOUR_GREEN	= "\033[0;32m"
+COLOUR_RESET	= "\033[0;0m"
+COLOUR_BOLD		= "\033[;1m"
+COLOUR_REVERSE	= "\033[;7m"
+
+DARE2SHARE_HEADER = """
+#########################################################################
+#                                                                       #
+#      #                                                   ######       #
+#      #                                                 ##      ##     #
+#      #                                                ##        ##    #
+#      #                                                ############    #
+#   ####  ####   ####  ###      ###                           #         #
+#  #   #      # #     #   #    #   #         #                #         #
+# #    #  ### # #     # ##    #     #        #               ##         #
+#  #   # #    # #     #       #     #        #                          #
+#   ####  ##### #      ####        #         #                          #
+#                                 #    ####  # ###   ####   ####  ###   #
+#                               #     #      #    #      # #     #   #  #
+#                              #       ####  #    #  ### # #     # ##   #
+#                             #            # #    # #    # #     #      #
+#                             ######   ####  #    #  ##### #      ####  #
+#                                                                       #
+#########################################################################
+#                                                                       #
+# BLUETOOTH LOW ENERGY (IBEACON) TRACKING                               #
+#                                                                       #
+#########################################################################
+#                                                                       #
+# Project: dare2share - Junge Akademie - Technical University of Munich #
+# Authors: David Wei, Gunther Bidlingmaier, Max Bauer                   #
+# Date:    16/05/2017                                                   #
+# Version: 0.0                                                          #
+# Mail:    dare2share@jungeakademie.tum.de                              #
+# Python:  2.7                                                          #
+#                                                                       #
+#########################################################################
+"""
+
+def printHeader():
+	print(COLOUR_BLUE)
+	print(DARE2SHARE_HEADER)
+	print(COLOUR_RESET)
