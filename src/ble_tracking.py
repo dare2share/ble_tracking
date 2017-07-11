a61399441de7b126da0a7998387ef31df09b75b0
david.wei <david.wei@warr.de>
2017-07-11 19:22:52 +0200

    Add argparse stationID

--- src/ble_tracking.py
+++ src/ble_tracking.py
@@ -2,6 +2,7 @@
 
 import beaconscanner as bs
 import beaconlogger as bl
+import argparse
 import dare2share_header as d2sh
 
 d2sh.printHeader()
@@ -17,10 +18,21 @@ for bd_item in myBeaconData:
 	print(bd_item)
 """
 ####################
-
+"""
 print("TEST #2 (beacon logger)")
 myBeaconLogger = bl.BeaconLogger("/home/pi/data/", station_ID = "myStationID", scan_duration = 3, log_config = bl.bs.bd.BD_LOG_CONFIG_DEFAULT)
 myBeaconLogger.run()
+"""
+####################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("stationID", help = "unique name of the sharing station")
+args = parser.parse_args()
+stationID = args.stationID
+print("Initiating tracking as station \"" + stationID + "\"")
+
+beaconLogger = bl.BeaconLogger("data/", station_ID = stationID, scan_duration = 3, log_config = bl.bs.bd.BD_LOG_CONFIG_DEFAULT)
+beaconLogger.run()
 
 ####################
 
