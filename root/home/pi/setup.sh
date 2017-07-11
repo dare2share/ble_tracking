58a388a93726ece5b5ca7f633ce5fb5b6c84e31b
david.wei <david.wei@warr.de>
2017-07-11 20:32:04 +0200

    Add setup script

---
+++ root/home/pi/setup.sh
@@ -0,0 +1,9 @@
+echo run as root
+
+systemctl enable unord.ble_tracking.service
+systemctl enable usued.ble_tracking.service
+systemctl enable mensa.ble_tracking.service
+systemctl enable mi.ble_tracking.service
+systemctl enable mw.ble_tracking.service
+systemctl enable ph.ble_tracking.service
+systemctl enable ch.ble_tracking.service
