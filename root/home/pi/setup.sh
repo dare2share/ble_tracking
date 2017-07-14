88aeb339c29cbf572afc24184edfa1017564d716
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2017-07-14 15:20:05 +0200

    Correct setup file

--- root/home/pi/setup.sh
+++ root/home/pi/setup.sh
@@ -1,10 +1,14 @@
 echo run as root
 echo Example usage: "bash setup.sh ch", get abbreviations from pi_to_port
+
 #copy correct service file
 cp /home/pi/ble_tracking/root/lib/systemd/system/$1.ble_tracking.service /lib/systemd/system/
 systemctl enable $1.ble_tracking.service
+systemctl start $1.ble_tracking.service
+
 #get correct port from mapping file
-port=$(grep -Po "(?<=^$1 ).*" ~/home/pi/ble_tracking/root/home/pi/pi_to_port)
+port=$(grep -Po "(?<=^$1 ).*" /home/pi/ble_tracking/root/home/pi/pi_to_port)
+
 #write script for ssh-tunneling
 cat > /usr/bin/ssh-tunneling << EOL
 #!/bin/sh
