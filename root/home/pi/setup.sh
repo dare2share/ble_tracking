4edf8cb2f1253179e3ae1c326d8f4c950a6f5a46
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2017-07-14 15:11:45 +0200

    Adding ssh-tunneling setup to setup script

--- root/home/pi/setup.sh
+++ root/home/pi/setup.sh
@@ -1,3 +1,12 @@
 echo run as root
+echo Example usage: "bash setup.sh ch", get abbreviations from pi_to_port
+#copy correct service file
 cp /home/pi/ble_tracking/root/lib/systemd/system/$1.ble_tracking.service /lib/systemd/system/
 systemctl enable $1.ble_tracking.service
+#get correct port from mapping file
+port=$(grep -Po "(?<=^$1 ).*" ~/home/pi/ble_tracking/root/home/pi/pi_to_port)
+#write script for ssh-tunneling
+cat > /usr/bin/ssh-tunneling << EOL
+#!/bin/sh
+autossh -M 20000 -f -N di36him@webdev-tum.lrz.de -R $port:localhost:22 -C
+EOL
